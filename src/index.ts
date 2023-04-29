e1b99351350ef42d12a23663fa39c7367ebc25ec
leoo <leoo@synology.com>
2023-04-29 00:26:10 -0700

    adds oauth2 boilerplate

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,24 @@
-import express from "express";
+import express from 'express';
+import mongoose from 'mongoose';
+import EventEmitter from 'events';
+import { auth } from 'express-oauth2-jwt-bearer';
+// import cookieParser from 'cookie-parser';
 
 const app = express();
-const port = 8080; // default port to listen
+const port = process.env.PORT || 8080; // default port to listen
+
+const jwtCheck = auth({
+    audience: process.env.AUDIENCE,
+    issuerBaseURL: process.env.ISSUER_BASE_URL,
+    tokenSigningAlg: 'RS256'
+  });
+
+// enforce on all endpoints
+app.use(jwtCheck);
 
 // define a route handler for the default home page
-app.get( "/", ( req, res ) => {
-    res.send( "Hello world!" );
+app.get( '/', ( req, res ) => {
+    res.send( 'Hello world!' );
 } );
 
 // start the Express server
