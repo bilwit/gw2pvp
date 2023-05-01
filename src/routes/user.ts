e38b65d3b271fa664c842e67ef41701fe9437ad0
leoo <leoo@synology.com>
2023-04-30 22:04:16 -0700

    renames validateJwt

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import { User } from '../models/User';
+import { validateJwt } from '../services/validateJwt';
 
 export const userRouter = express.Router();
 
 // /api/user
-userRouter.post('/', async (req, res) => {
+userRouter.post('/', validateJwt, async (req, res) => {
   
 });
--- src/services/validateJwt.ts
+++ src/services/validateJwt.ts
@@ -1,7 +1,7 @@
 import { auth } from 'express-oauth2-jwt-bearer';
 
 // initialize oath2 authentication
-export const checkJwt = auth({
+export const validateJwt = auth({
     audience: process.env.AUDIENCE,
     issuerBaseURL: process.env.ISSUER_BASE_URL,
     tokenSigningAlg: 'RS256'
